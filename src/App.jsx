1441b37f2b8c4aa2a484f22fe6ee22f5aa068c70
unknown <victor.90braz@gmail.com>
2022-12-25 03:20:50 +0100

    Added the ground with a rotation and position

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
+import Ground from "./components/Ground/Ground.jsx";
 
-const App = () => {
+function App() {
   return (
     <Canvas>
       <Sky sunPosition={[100, 100, 20]} />
       <ambientLight intensity={0.5} />
-      <Physics />
+      <Physics>
+        <Ground />
+      </Physics>
     </Canvas>
   );
-};
+}
 
 export default App;
