caadd9c6dcb95613da869dcd08729557fc848a04
unknown <victor.90braz@gmail.com>
2023-01-01 22:30:49 +0100

    Added images to textures

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,8 +1,19 @@
-import { grassImg } from "./images";
-import { RepeatWrapping, TextureLoader } from "three";
+import { grassImg, dirtImg, logImg, glassImg, woodImg } from "./images";
+import { NearestFilter, RepeatWrapping, TextureLoader } from "three";
 
 const groundTexture = new TextureLoader().load(grassImg);
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+
 groundTexture.wrapS = RepeatWrapping;
 groundTexture.wrapT = RepeatWrapping;
 
-export { groundTexture };
+groundTexture.magFilter = NearestFilter;
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+
+export { groundTexture, dirtTexture, logTexture, glassTexture, woodTexture };
